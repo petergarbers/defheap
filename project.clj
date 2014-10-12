69f5625c0c709f48659e45f466fc3c4f3b754cbd
Peter Garbers <peter.garbers@gmail.com>
2014-10-12 14:29:01 -0400

    lets rock this

---
+++ project.clj
@@ -0,0 +1,23 @@
+(defproject defheap "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2311"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [racehub/om-bootstrap "0.3.0"]
+                 [om "0.7.1"]
+                 [sablono "0.2.22"]]
+
+  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
+
+  :source-paths ["src/cljs"]
+  :resource-paths ["resources"]
+  :cljsbuild {
+    :builds [{:id "dev"
+              :source-paths ["src/cljs"]
+              :compiler {
+                :output-to "resources/public/html/js/defheap.js"
+                :output-dir "resources/public/html/js/out"
+                :optimizations :none
+                :source-map true}}]})
